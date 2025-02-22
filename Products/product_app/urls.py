0c9a30b11539c43be7caf8a29cb2f8a591df98ab
AayushKThakur <connecttoakt@gmail.com>
2025-02-22 15:12:41 +0530

    class material

--- Products/product_app/urls.py
+++ Products/product_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('products/', views.all_products, name = 'all_products')
+    path('prdocuts/<int:product_id>/', views.products_details_by_id, name = "products_by_id"),
+    path("products/name/<str:product_name>/", views.prodct_by_name, name = "product_by_name")
 
 ]
--- Products/product_app/views.py
+++ Products/product_app/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse,JsonResponse,Http404
 # Create your views here.
 
 products={
-    1:{'name' : 'A', 'price' : 1500},
-    2:{'name' : 'B', 'price' : 1100},
+    1:{'name' : 'laptop', 'price' : 1500},
+    2:{'name' : 'charger', 'price' : 1100},
     3:{'name' : 'C', 'price' : 150},
 }
 
@@ -15,3 +15,30 @@ def home(request):
 def all_products(request):
     product_items={product['name']: product['price'] for product in products.values() }
     return JsonResponse(product_items)
+
+#dynamic url
+def products_details_by_id(requestm product_id):
+    product = products.get(product_id)
+    if product:
+        return HttpResponse(
+            f"<h1>Product Details</h1>"
+            f"<p>Na me: {product['name</p>"
+            f"<p>Product Details</p>"
+
+        )
+    else:
+        raise Http404(f"Product with ID {product_id} not found")
+
+#for string
+def products_details_by_id(request, product_name):
+    for product in products.values():
+        if product['name']==product_name:
+            return HttpResponse(
+                f"<h1> Product Details</h1>"
+                f"<p> Name: {product['name']}</p>"
+                f"<p> Name: {product['price']}</p>"
+            )
+        return HttpResponse(f"<h1>Product with name: {product_name} not found</h1>")
+
+def filter_products(request):
+    max_price = request.GET.get('max_price')
